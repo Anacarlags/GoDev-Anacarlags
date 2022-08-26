88d1c8a459b69be19c12f01073a1505b92ab9445
Ana Carla gomes <32022053+Anacarlags@users.noreply.github.com>
2022-08-26 10:26:45 -0300

    fx:parte um pronta/parte 2 endpoints null

---
+++ godevsenior/src/main/java/com/godevsenior/godevsenior/GodevseniorApplication.java
@@ -0,0 +1,13 @@
+package com.godevsenior.godevsenior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodevseniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodevseniorApplication.class, args);
+	}
+
+}
